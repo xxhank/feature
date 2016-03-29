17f35ef420f9ddb86a1cfb953b70096f00c24eab
wangchao <xxhank.boy@gmail.com>
2016-03-30 07:01:29 +0800

    tableview功能演示

---
+++ TableViewDemo/TableViewDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TableViewDemo
+//
+//  Created by wangchaojs02 on 16/3/30.
+//  Copyright © 2016年 wangchaojs02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var feedListView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+ 
