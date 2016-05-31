d200a1d88e02ab5b07cd7a0fac15d54b552bf022
王超9 <wangchao9@le.com>
2016-05-31 19:24:42 +0800

    实现DownloadManager的队列管理

--- DownloadManager/DownloadManager/DownloadEngine/DownloadManager.swift
+++ DownloadManager/DownloadManager/DownloadEngine/DownloadManager.swift
@@ -8,6 +8,62 @@
 
 import UIKit
 
-class DownloadManager: NSObject {
+public class DownloadTask {
+    public var key: String = ""
+}
+
+public class DownloadManager: NSObject {
+    class func manager(database: String) -> DownloadManager {
+        return DownloadManager(database: database)
+    }
+
+    public var numberOfTasks: Int {
+        return tasks.count
+    }
+
+    var tasks: [DownloadTask] = []
+
+    init(database: String) {
+
+    }
+
+    public func append(task: DownloadTask, allowDuplicate: Bool = false) {
+        if allowDuplicate || taskWithKey(task.key) == nil {
+            tasks.append(task)
+        }
+    }
+
+    public func remove(task: DownloadTask) {
+        if let index = indexOfTask(task) {
+            tasks.removeAtIndex(index)
+        }
+    }
+
+    func indexOfTask(task: DownloadTask) -> Int? {
+        return tasks.indexOf({ (task_) -> Bool in
+            return task_ === task
+        })
+    }
+
+    func indexOfTask(forKey key: String) -> Int? {
+        return tasks.indexOf({ (task_) -> Bool in
+            return task_.key == key
+        })
+    }
+
+    public func taskWithKey(key: String) -> DownloadTask? {
+        if let index = indexOfTask(forKey: key) {
+            return tasks[index]
+        }
+        return nil
+    }
+
+    func taskAtIndex(index: Int) -> DownloadTask? {
+        if index < tasks.count {
+            return tasks[index]
+        } else {
+            return nil
+        }
+    }
 
 }
--- DownloadManager/DownloadManagerTests/DownloadManagerTests.swift
+++ DownloadManager/DownloadManagerTests/DownloadManagerTests.swift
@@ -10,27 +10,45 @@ import XCTest
 @testable import DownloadManager
 
 class DownloadManagerTests: XCTestCase {
-    
+    let manager = DownloadManager.manager("test.db")
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    // MARK: - queue manager
+    func testLoadFromNotExistDatabase() {
+
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+
+    func testQueueManager() {
+        let count = manager.numberOfTasks
+        let task = DownloadTask()
+        manager.append(task)
+        XCTAssertEqual(count + 1, manager.numberOfTasks)
+
+        manager.remove(task)
+        XCTAssertEqual(count, manager.numberOfTasks)
+
+        manager.append(task)
+        let task2 = DownloadTask()
+        manager.append(task2)
+        XCTAssertEqual(count + 1, manager.numberOfTasks)
+
+        manager.append(task2, allowDuplicate: true)
+        XCTAssertEqual(count + 2, manager.numberOfTasks)
+
+        if let query = manager.taskAtIndex(count) {
+            XCTAssert(query === task)
+        } else {
+            XCTAssert(false)
         }
+
+        XCTAssert(manager.taskAtIndex(manager.numberOfTasks + 1) == nil)
     }
-    
 }
