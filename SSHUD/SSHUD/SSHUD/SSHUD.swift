74a30f405ee83a7f39807b7c492bf68a90cd118c
wangchao <xxhank.boy@gmail.com>
2016-06-03 06:35:21 +0800

    重新命名

--- SSHUD/SSHUD/SSHUD/SSHUD.swift
+++ SSHUD/SSHUD/SSHUD/SSHUD.swift
@@ -8,58 +8,231 @@
 
 import UIKit
 
-public enum HUDHide {
+public enum HUDDisappear {
     case Auto
     case After(delay: NSTimeInterval)
     case Manual
     case Never
 }
 
-public enum HUDPosition {
+public typealias HUDLayoutMethod = (canvas: UIView, content: UIView) -> Void
+public enum HUDLayout {
     case Top
     case Center
     case Bottom
+    case Custom(customeLayout: HUDLayoutMethod)
 }
 
-public class SSHUD: NSObject {
-    static let hud = SSHUD()
-    class func showHUD(model model: Bool, hide: HUDHide, position: HUDPosition = HUDPosition.Center) {
-        SSHUD.hud.showHUD(model: model, hide: hide, position: position)
+func SSHUDTest() {
+    SSHUD.hud
+        .model(true)
+        .disappear(HUDDisappear.After(delay: 0.3))
+        .layout(HUDLayout.Center)
+        .show(animated: true)
+
+    SSHUD.hud.hide(animated: true)
+}
+
+public typealias HUDCompletion = (finished: Bool) -> Void
+extension SSHUD {
+    func model(model: Bool) -> Self {
+        self.model = model
+        return self
     }
+
+    func disappear(disappear: HUDDisappear) -> Self {
+        self.disappearMethod = disappear
+        return self
+    }
+
+    func layout(layout: HUDLayout) -> Self {
+        self.layout = layout
+        return self
+    }
+
+    func content(contentView: UIView) -> Self {
+        self.contentView = contentView
+        return self
+    }
+    func completion(completion: HUDCompletion) -> Self {
+        self.completion = completion
+        return self
+    }
+}
+public class SSHUD: NSObject {
+    var model = false
+    var layout = HUDLayout.Center
+    var contentView: UIView!
+    var disappearMethod = HUDDisappear.Auto
+    var completion: HUDCompletion?
+    var padding: CGFloat = 10
+    var navigationBarHeight: CGFloat = 64
+
     lazy var backgroundGesture: UITapGestureRecognizer = {
-        SSLogInfo("create background gesture")
         let gesture = UITapGestureRecognizer(target: self, action: #selector(tapBackground(_:)))
         gesture.delegate = self
         return gesture
     }()
 
     let window = HUDWindow()
-    lazy var contentView: UIView = {
-        let label = UILabel()
-        label.text = "Message"
-        label.textAlignment = .Center
-        label.textColor = UIColor.whiteColor()
-        return label
-    }()
-    var hideMethod = HUDHide.Auto
+
     var autoHideTimer: NSTimer? = nil
+    var animationDuration = 0.3
+
+    static let hud = SSHUD()
+    class func showHUD(model model: Bool, hide: HUDDisappear, position: HUDLayout = HUDLayout.Center) {
+        SSHUD.hud.showHUD(model: model, disappear: hide, layout: position)
+    }
 
     override init() {
         super.init()
+
+        window.backgroundColor = UIColor.clearColor()
+        window.addGestureRecognizer(backgroundGesture)
+        window.layout = { [weak self] canvas, content in
+            self?.layoutContentView(canvas, content: content)
+        }
+
+        observeStatusBarRotation()
+    }
+
+    deinit {
+        ignoreStatusBarRotation()
+    }
+
+    func showHUD(model model: Bool, disappear: HUDDisappear, layout: HUDLayout = HUDLayout.Center) {
+        let contentView = UILabel()
+        contentView.textColor = UIColor.whiteColor()
+        contentView.text = "Message"
+        contentView.backgroundColor = UIColor(white: 0, alpha: 0.6)
+
+        contentView.frame = contentView.textRectForBounds(window.bounds, limitedToNumberOfLines: 3)
+        self.model(model)
+            .content(contentView)
+            .disappear(disappear)
+            .layout(layout)
+            .show(animated: true)
+    }
+
+    func hideHUD(animated animated: Bool = true) {
+        hide(animated: animated)
+    }
+
+    func prepare() {
+        setupAutoHideTimer(self.disappearMethod)
+
+        window.addSubview(contentView)
+        window.contentView = contentView
+        window.makeKeyAndVisible()
+
+        rorateContentView()
+    }
+
+    func show(animated animated: Bool) {
+        hide(animated: false)
+
+        prepare()
+
+        if animated {
+            window.alpha = 0
+            UIView.animateWithDuration(animationDuration, animations: {
+                self.window.alpha = 1
+            }) { (finished) in
+                self.window.alpha = 1
+            }
+        } else {
+        }
+    }
+
+    func hide(animated animated: Bool) {
+        if animated {
+            UIView.animateWithDuration(animationDuration, animations: {
+                self.window.alpha = 0
+            }) { (finished) in
+                self.contentView.removeFromSuperview()
+                self.window.alpha = 0
+                self.window.hidden = true
+            }
+        } else {
+            self.contentView.removeFromSuperview()
+            self.window.alpha = 0
+            self.window.hidden = true
+        }
+        autoHideTimer?.invalidate()
+        autoHideTimer = nil
+    }
+
+    func tapBackground(gesture: AnyObject) {
+        SSLogInfo("tapBackground")
+        switch disappearMethod {
+        case .Manual:
+            hideHUD(animated: true)
+        default: break
+        }
+    }
+
+    func setupAutoHideTimer(hide: HUDDisappear) {
+        self.disappearMethod = hide
+        autoHideTimer?.invalidate()
+
+        switch hide {
+        case .After(let delay):
+            autoHideTimer = NSTimer.scheduledTimerWithTimeInterval(delay, target: self, selector: #selector(autoHideTimerFired), userInfo: nil, repeats: false)
+        case .Auto:
+            autoHideTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(autoHideTimerFired), userInfo: nil, repeats: false)
+        default: break
+        }
+    }
+
+    func autoHideTimerFired() {
+        hideHUD(animated: true)
+        autoHideTimer = nil
+    }
+}
+
+// MARK: - UIGestureRecognizerDelegate
+extension SSHUD: UIGestureRecognizerDelegate {
+    public func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer == backgroundGesture {
+            let point = gestureRecognizer.locationInView(contentView)
+            if contentView.bounds.contains(point) {
+                return false
+            } else {
+                return true
+            }
+        }
+        return true
+    }
+}
+// MARK: - Observe Status Bar Rotation
+extension SSHUD {
+    func observeStatusBarRotation() {
         NSNotificationCenter.defaultCenter()
-            .addObserver(self, selector: #selector(statusBarRotated(_:)), name: UIApplicationDidChangeStatusBarOrientationNotification, object: nil)
+            .addObserver(self,
+                selector: #selector(statusBarRotated(_:)),
+                name: UIApplicationDidChangeStatusBarOrientationNotification,
+                object: nil)
+    }
+
+    func ignoreStatusBarRotation() {
+        NSNotificationCenter.defaultCenter()
+            .removeObserver(self,
+                name: UIApplicationDidChangeStatusBarOrientationNotification,
+                object: nil)
     }
 
     func statusBarRotated(notification: NSNotification) {
-        updateContentView()
+        rorateContentView()
     }
 
-    func updateContentView() {
-        SSLogInfo("\(UIApplication.sharedApplication().statusBarOrientation.rawValue)")
-        SSLogInfo("\(UIDevice.currentDevice().orientation.rawValue)")
-        let orientation = UIApplication.sharedApplication().statusBarOrientation
+    func rorateContentView() {
+        let statusBarOrientation = UIApplication.sharedApplication().statusBarOrientation
         let deviceOrientation = UIDevice.currentDevice().orientation
-        switch orientation {
+
+        SSLogInfo("\(statusBarOrientation)")
+        SSLogInfo("\(deviceOrientation)")
+
+        switch statusBarOrientation {
         case .LandscapeLeft:
             if deviceOrientation != UIDeviceOrientation.LandscapeRight {
                 let angle = -CGFloat(M_PI_2)
@@ -83,170 +256,19 @@ public class SSHUD: NSObject {
         default: break
         }
     }
-
-    func showHUD(model model: Bool, hide: HUDHide, position: HUDPosition = HUDPosition.Center) {
-        self.hideMethod = hide
-        autoHideTimer?.invalidate()
-
-        switch hide {
-        case .After(let delay):
-            autoHideTimer = NSTimer.scheduledTimerWithTimeInterval(delay, target: self, selector: #selector(hideHUD), userInfo: nil, repeats: false)
-        case .Auto:
-            autoHideTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(hideHUD), userInfo: nil, repeats: false)
-        default: break
-        }
-
-        contentView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        contentView.backgroundColor = UIColor(white: 0, alpha: 0.8)
-        window.addSubview(contentView)
-        window.position = position
-        updateContentView()
-        if model {
-            window.backgroundColor = UIColor(white: 0, alpha: 0.1)
-        } else {
-            window.backgroundColor = UIColor.clearColor()
-        }
-
-        window.alpha = 0
-
-        UIView.animateWithDuration(0.2, animations: {
-            self.window.alpha = 1
-        }) { (finished) in
-            self.window.alpha = 1
-        }
-
-        window.model = model
-        window.view = contentView
-
-        window.addGestureRecognizer(backgroundGesture)
-        window.makeKeyAndVisible()
-    }
-
-    func hideHUD() {
-        contentView.removeFromSuperview()
-        window.hidden = true
-    }
-
-    func tapBackground(gesture: AnyObject) {
-        SSLogInfo("tapBackground")
-        switch hideMethod {
-        case .Manual:
-            hideHUD()
-        default: break
-        }
-    }
-
-    func autoHideTimerFired() {
-        autoHideTimer = nil
-    }
 }
 
-// MARK: - UIGestureRecognizerDelegate
-extension SSHUD: UIGestureRecognizerDelegate {
-    public func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
-        if gestureRecognizer == backgroundGesture {
-            let point = gestureRecognizer.locationInView(contentView)
-            if contentView.bounds.contains(point) {
-                return false
-            } else {
-                return true
-            }
-        }
-        return true
-    }
-}
-
-class HUDWindow: UIWindow {
-    var model = false
-    var view: UIView? = nil
-    var position = HUDPosition.Center
-    var padding: CGFloat = 10
-    var navigationBarHeight: CGFloat = 64
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setup()
-    }
-
-    func setup() {
-        self.rootViewController = HUDViewController()
-    }
-
-    override func hitTest(point: CGPoint, withEvent event: UIEvent?) -> UIView? {
-        guard let view = view else { return nil }
-        if view.frame.contains(point) {
-            return view
-        }
-        return model ? self : nil
-    }
-
-    override func layoutSubviews() {
-        guard let contentView = view else { return }
-        let window = self
-
-        switch position {
+extension SSHUD {
+    func layoutContentView(canvas: UIView, content: UIView) {
+        switch layout {
         case .Top:
             contentView.center = CGPoint(x: window.center.x, y: navigationBarHeight + padding + contentView.bounds.height * 0.5)
         case .Center:
             contentView.center = window.center
         case .Bottom:
             contentView.center = CGPoint(x: window.center.x, y: window.bounds.maxY - (padding + contentView.bounds.height * 0.5))
+        case .Custom(let customeLayout):
+            customeLayout(canvas: window, content: contentView)
         }
-    }
-}
-
-/// 适配当前屏幕方向
-class HUDViewController: UIViewController {
-    override func loadView() {
-        self.view = nil
-    }
-
-    internal override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if let rootViewController = self.rootViewController {
-            return rootViewController.supportedInterfaceOrientations()
-        } else {
-            return UIInterfaceOrientationMask.Portrait
-        }
-    }
-
-    internal override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        if let rootViewController = self.rootViewController {
-            return rootViewController.preferredStatusBarStyle()
-        } else {
-            return .Default
-        }
-    }
-
-    internal override func prefersStatusBarHidden() -> Bool {
-        if let rootViewController = self.rootViewController {
-            return rootViewController.prefersStatusBarHidden()
-        } else {
-            return false
-        }
-    }
-
-    internal override func preferredStatusBarUpdateAnimation() -> UIStatusBarAnimation {
-        if let rootViewController = self.rootViewController {
-            return rootViewController.preferredStatusBarUpdateAnimation()
-        } else {
-            return .None
-        }
-    }
-
-    internal override func shouldAutorotate() -> Bool {
-        if let rootViewController = self.rootViewController {
-            return rootViewController.shouldAutorotate()
-        } else {
-            return false
-        }
-    }
-
-    private var rootViewController: UIViewController? {
-        return UIApplication.sharedApplication().delegate?.window??.rootViewController
     }
 }
